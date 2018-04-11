38f1e484a824df2333d2d2348fcf1e7b42eaa280
fxk01 <2416113928@qq.com>
2018-04-11 17:35:27 +0800

    route

--- src/router/index.js
+++ src/router/index.js
@@ -4,22 +4,43 @@
 
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../page/home/home.vue'
-import List from '../page/list/list.vue'
+// import Home from '../page/home/home.vue'
+// import List from '../page/list/list.vue'
 
 Vue.use(Router);
 
+const home = r => require.ensure([], () => r(require('@/page/home/home.vue')), 'home');
+const list = r => require.ensure([], () => r(require('@/page/list/list.vue')), 'list');
+
+const routes = [
+  {
+    path: '/',
+    component: home
+  },
+  {
+    path: '/list',
+    component: list
+  }
+];
+
+// export default new Router({
+//   mode: 'history',
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: Home
+//     },
+//     {
+//       path: '/list',
+//       name: 'list',
+//       component: List
+//     }
+//   ]
+// })
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/list',
-      name: 'list',
-      component: List
-    }
-  ]
+  mode: 'history',
+  routes,
+  strict: process.env.NODE_ENV !== 'production',
 })
